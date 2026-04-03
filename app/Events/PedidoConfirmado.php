9c8902167bf57c8d4192ddd210d8ad27fc912b55
Tecnobyte360 <comercial@tecnobyte360.com>
2026-04-02 22:31:16 -0500

    s

--- app/Events/PedidoConfirmado.php
+++ app/Events/PedidoConfirmado.php
@@ -5,11 +5,11 @@ namespace App\Events;
 use App\Models\Pedido;
 use Illuminate\Broadcasting\Channel;
 use Illuminate\Broadcasting\InteractsWithSockets;
-use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcastNow;
 use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Queue\SerializesModels;
 
-class PedidoConfirmado implements ShouldBroadcast
+class PedidoConfirmado implements ShouldBroadcastNow
 {
     use Dispatchable, InteractsWithSockets, SerializesModels;
 
