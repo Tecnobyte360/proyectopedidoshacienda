21c0754aff8966e7825580cda1524290b9993fd6
Tecnobyte360 <comercial@tecnobyte360.com>
2026-04-02 22:59:36 -0500

    s

--- resources/views/pedidos/index.blade.php
+++ resources/views/pedidos/index.blade.php
@@ -23,7 +23,7 @@
         wsPort: 443,
         wssPort: 443,
         forceTLS: true,
-        enabledTransports: ['wss'],
+       enabledTransports: ['ws', 'wss'],
         disableStats: true,
     });
 
