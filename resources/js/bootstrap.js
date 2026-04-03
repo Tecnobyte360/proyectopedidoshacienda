e521095095c2ba91819066660922d7b866f699fb
Tecnobyte360 <comercial@tecnobyte360.com>
2026-04-02 21:14:19 -0500

    se realiza ajuste en resource.js

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -1,19 +1,20 @@
 import axios from 'axios';
+import Echo from 'laravel-echo';
+import Pusher from 'pusher-js';
 
 window.axios = axios;
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
-import Echo from 'laravel-echo';
-import Pusher from 'pusher-js';
-
 window.Pusher = Pusher;
+
+const isHttps = (import.meta.env.VITE_REVERB_SCHEME ?? 'https') === 'https';
 
 window.Echo = new Echo({
     broadcaster: 'reverb',
     key: import.meta.env.VITE_REVERB_APP_KEY,
-    wsHost: import.meta.env.VITE_REVERB_HOST ?? window.location.hostname,
-    wsPort: import.meta.env.VITE_REVERB_PORT ?? 8080,
-    wssPort: import.meta.env.VITE_REVERB_PORT ?? 8080,
-    forceTLS: (import.meta.env.VITE_REVERB_SCHEME ?? 'http') === 'https',
+    wsHost: import.meta.env.VITE_REVERB_HOST || window.location.hostname,
+    wsPort: isHttps ? 80 : Number(import.meta.env.VITE_REVERB_PORT || 80),
+    wssPort: isHttps ? 443 : Number(import.meta.env.VITE_REVERB_PORT || 443),
+    forceTLS: isHttps,
     enabledTransports: ['ws', 'wss'],
 });
